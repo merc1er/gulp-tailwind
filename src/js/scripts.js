9ae08eff63291e727274d2f535d700106fa377b9
Corentin Mercier <corentin@mercier.link>
2020-12-01 17:14:33 +0700

    Create mobile menu

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,1 +1,10 @@
 // Custom JavaScript
+
+// Menu
+
+const menu      = document.getElementById("menu");
+const openMenu  = document.getElementById("openMenu");
+const closeMenu = document.getElementById("closeMenu");
+
+openMenu.onclick = function () {console.log(menu.classList.remove("hidden"));};
+closeMenu.onclick = function () {console.log(menu.classList.add("hidden"));};
